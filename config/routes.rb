75a33caf6abeaaea8d7c94c424e9ba4f44ca4d5e
tochisuke221 <tochisuke221@gmail.com>
2021-03-27 22:44:11 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users,only:[:update,:edit,:show]
-  resources :rooms,only:[:new,:create]
+  resources :rooms,only:[:new,:create] do
+    resources :messages,only:[:index,:create]
+  end
+  
 end
